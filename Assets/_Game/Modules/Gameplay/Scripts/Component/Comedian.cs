cbfb49751b9d399b6d97394e7f7d4c263cee26c9
firatozbay <firatozbay94@gmail.com>
2024-01-28 11:54:14 +0300

    Update Comedian class

--- Assets/_Game/Modules/Gameplay/Scripts/Component/Comedian.cs
+++ Assets/_Game/Modules/Gameplay/Scripts/Component/Comedian.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Library.SignalBusSystem;
+using Modules.Gameplay;
+using Library.ServiceLocatorSystem;
+using DG.Tweening;
 
 public class Comedian : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private InputController InputController => ServiceLocator.Get<InputController>();
+
+    private Transform _child;
+
+    private Vector3 _jumpPosition;
+
+
     void Start()
     {
-        
+        _child = transform.GetChild(0);
+
+        SignalBus.Subscribe<MusicKeyBlastedSignal>(OnMusicKeyBlasted);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMusicKeyBlasted(MusicKeyBlastedSignal signal)
     {
-        
+        _child.DOLocalMove(_jumpPosition, 0.1f);
     }
 }
